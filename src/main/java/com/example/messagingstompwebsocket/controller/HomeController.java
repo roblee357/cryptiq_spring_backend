071b0eb07f6dcc1d67ed1bbb0f047419be957459
roblee357 <roblee357@hotmail.com>
2021-10-27 06:17:50 -0400

    changes

--- src/main/java/com/example/messagingstompwebsocket/controller/HomeController.java
+++ src/main/java/com/example/messagingstompwebsocket/controller/HomeController.java
@@ -2,13 +2,14 @@ package com.example.messagingstompwebsocket.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.messagingstompwebsocket.model.Message;
-
+@Controller
 public class HomeController {
 
 	private Logger logger = LoggerFactory.getLogger(MessageController.class);
