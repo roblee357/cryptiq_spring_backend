2a164d25487f2371a854b8a3dbca8fb890f95dad
roblee357 <roblee357@hotmail.com>
2021-10-27 06:03:19 -0400

    changes

---
+++ src/main/java/com/example/messagingstompwebsocket/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.messagingstompwebsocket.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.messagingstompwebsocket.model.Message;
+
+public class HomeController {
+
+	private Logger logger = LoggerFactory.getLogger(MessageController.class);
+	
+	@GetMapping(path="/home") // Map ONLY POST Requests
+	public @ResponseBody String addNewMessage () {
+		
+		logger.info("Successful test of Cryptiq");
+		return "Successful test of Cryptiq";
+		
+	}
+}
